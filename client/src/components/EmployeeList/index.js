d28e7f96f947edc9c3d5ba83593779bc5b585b4f
NatnaelSisay <natyjava8@gmail.com>
2021-01-12 11:26:22 +0300

    make a button styled

--- client/src/components/EmployeeList/index.js
+++ client/src/components/EmployeeList/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import Button from "../UI/Button";
 const EmployeeList = (props) => {
     return (
         <div>
             Employee List
-            <Link to="/edit">Edit</Link>
+            <Link to="/edit">
+                <Button>Edit</Button>
+            </Link>
         </div>
     );
 };

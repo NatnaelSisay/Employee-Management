1927b38c5c6602bbfe991cf7fb17b7a8c10c448a
NatnaelSisay <natyjava8@gmail.com>
2021-01-12 11:14:12 +0300

    check all the routes work properly

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./index.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 
 import EmployeeList from "../EmployeeList";
 import AddEmployee from "../AddEmployee";
@@ -13,25 +13,34 @@ function App() {
     return (
         <div className="App">
             <header className="header">
-                <h1>Employee Management</h1>
+                <h1>
+                    <Link to="/" className="link">
+                        {" "}
+                        Employee Management
+                    </Link>
+                </h1>
                 <div>
-                    <button type="button">Add +</button>
+                    <button>
+                        <Link className="link" to="/add">
+                            {" "}
+                            Add +{" "}
+                        </Link>
+                    </button>
                 </div>
             </header>
-            <Router>
-                <Switch>
-                    <Route path="/" exact>
-                        <EmployeeList />
-                    </Route>
-                    <Route path="/add" exact>
-                        <AddEmployee />
-                    </Route>
 
-                    <Route path="/edit">
-                        <EditEmployee />
-                    </Route>
-                </Switch>
-            </Router>
+            <Switch>
+                <Route path="/" exact>
+                    <EmployeeList />
+                </Route>
+                <Route path="/add" exact>
+                    <AddEmployee />
+                </Route>
+
+                <Route path="/edit">
+                    <EditEmployee />
+                </Route>
+            </Switch>
         </div>
     );
 }
--- client/src/components/EmployeeList/index.js
+++ client/src/components/EmployeeList/index.js
@@ -1,6 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 const EmployeeList = (props) => {
-    return <div>Employee List</div>;
+    return (
+        <div>
+            Employee List
+            <Link to="/edit">Edit</Link>
+        </div>
+    );
 };
 
 export default EmployeeList;

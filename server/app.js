f9961204c0aab279e5affc6f1a9e5311e5e8605c
NatnaelSisay <natyjava8@gmail.com>
2021-01-15 18:03:17 +0300

    seting up server

--- server/app.js
+++ server/app.js
@@ -1,43 +1,83 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+let fetchedData = [
+    {
+        id: 1,
+        employeeName: "Java The man",
+        dateOfBirth: "1919/23/2",
+        gender: "Male",
+        salary: "50,000",
+    },
+    {
+        id: 2,
+        employeeName: "Alemlate",
+        dateOfBirth: "1919/23/2",
+        gender: "Male",
+        salary: "50,000",
+    },
+    {
+        id: 3,
+        employeeName: "Jemal",
+        dateOfBirth: "1919/23/2",
+        gender: "Male",
+        salary: "50,000",
+    },
+    {
+        id: 4,
+        employeeName: "Temesgen",
+        dateOfBirth: "1919/23/2",
+        gender: "Male",
+        salary: "50,000",
+    },
+    {
+        id: 5,
+        employeeName: "Fifty",
+        dateOfBirth: "1919/23/2",
+        gender: "Male",
+        salary: "50,000",
+    },
 
+    {
+        id: 6,
+        employeeName: "Tedi",
+        dateOfBirth: "1919/23/2",
+        gender: "Male",
+        salary: "50,000",
+    },
+    {
+        id: 7,
+        employeeName: "Mere",
+        dateOfBirth: "1919/23/2",
+        gender: "Male",
+        salary: "50,000",
+    },
+];
 // the app is small scale so we dont need a router folder
 app.get("/", (req, res) => {
-  res.send({
-    id: 1,
-    name: "Natnael Sisay",
-    salary: "5000",
-    dateOfBirth: Date.now,
-    Gender: "Male",
-  });
+    res.header("Access-Control-Allow-Origin", "*");
+    res.send(fetchedData);
 });
 
 app.post("/", (req, res) => {
-  res.send({
-    id: 2,
-    name: "Java The Glorious",
-    salary: "5000",
-    dateOfBirth: Date.now,
-    Gender: "Male",
-  });
+    res.send(fetchedData);
 });
 
 app.delete("/:id", (req, res) => {
-  res.send({
-    message: "Deletion was successfull",
-  });
+    res.send({
+        message: "Deletion was successfull",
+    });
 });
 
 app.patch("/:id", (req, res) => {
-  res.send({
-    message: "Update was successfull",
-  });
+    res.send({
+        message: "Update was successfull",
+    });
 });
 
 app.get("*", (req, res) => {
-  res.send("404 page not found");
+    res.send("404 page not found");
 });
 app.listen(port, () => {
-  console.log(`Java app listening at http://localhost:${port}`);
+    console.log(`Java app listening at http://localhost:${port}`);
 });
